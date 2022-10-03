2359ae644bd3ca900702492af70dfcb003f6b4f2
Max Shylenko <maxshylenko@gmail.com>
2022-10-03 16:22:26 +0200

    start-page implemented and added app navigation using react-router v6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import Todos from './components/Todos';
-import DisplayTodos from './components/DisplayTodos';
+import React from 'react';
+import Start from './components/Start';
+import TodoPage from './components/TodoPage';
 import './styles/main.css';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1 className='Page-title'>TODO App</h1>
-      <Todos />
-      <DisplayTodos />
+      <Routes>
+        <Route exact path='/' element={<Start />} />
+        <Route exact path='/todos' element={<TodoPage />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export const Start = () => {
+const Start = () => {
+  const [name, setName] = useState('');
+  const navigate = useNavigate();
+
+  const clickHandler = (event) => {
+    if (name === '') {
+      alert('Name is empty!!! Please, provide your name!');
+    } else {
+      navigate('/todos', {
+        state: {
+          userName: name,
+        },
+      });
+    }
+  }
+
+  const handleChange = (event) => {
+    const { value } = event.target;
+
+    setName(value);
+  }
+
   return (
-    <div className="start-container">
-      <h1>Hello, Please enter name for start!</h1>
-      <input type="text" />
-      <button>Start</button>
+    <div className="Start-container">
+      <h1
+        className='Start-container__title'
+      >
+        Hello, Please enter your name for start!
+      </h1>
+
+      <input
+        className='Start-container__input'
+        type="text"
+        value={name}
+        onChange={handleChange}
+      />
+
+      <button
+        className='Start-container__button'
+        onClick={clickHandler}>
+        Start
+      </button>
     </div>
   )
 }
+
+export default Start;
