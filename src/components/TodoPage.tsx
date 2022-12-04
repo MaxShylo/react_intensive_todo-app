34e1f7d552e8d87dd97adb02f31e7c49395cf0ac
Max Shylenko <maxshylenko@gmail.com>
2022-12-04 23:53:41 +0100

    animation added, rectoring for form functionality

--- src/components/TodoPage.tsx
+++ src/components/TodoPage.tsx
@@ -2,16 +2,31 @@ import React from 'react';
 import Todos from './Todos';
 import DisplayTodos from './DisplayTodos';
 import { useLocation } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const TodoPage: React.FC = () => {
   const location = useLocation();
   const { userName } = location.state;
-  
+
   return (
     <>
-      <h1 className='Page-title'>{`${userName.toUpperCase()}\`s todos`}</h1>
-      <Todos />
-      <DisplayTodos />
+      <motion.h1
+        initial={{ y: -200 }}
+        animate={{ y: 0 }}
+        transition={{ type: "spring", duration: 0.5 }}
+        whileHover={{ scale: 1.1 }}
+        className='Page-title'
+      >
+        {`${userName.toUpperCase()}\`s todos`}
+      </motion.h1>
+      <motion.div
+        initial={{ y: 1000 }}
+        animate={{ y: 0 }}
+        transition={{ type: "spring", duration: 1 }}
+      >
+        <Todos />
+        <DisplayTodos />
+      </motion.div>
     </>
   )
 }
